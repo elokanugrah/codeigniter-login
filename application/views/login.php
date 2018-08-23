3e7fa59210333413b1e4c594ac41c34872e112b7
elokanugrah <33139266+elokanugrah@users.noreply.github.com>
2018-08-23 18:13:29 +0700

    db added

--- application/views/login.php
+++ application/views/login.php
@@ -4,8 +4,8 @@
 
       <fieldset class="clearfix">
 
-        <p><span class="fontawesome-user"></span><input type="text" name="username" onBlur="if(this.value == '') this.value = 'username'" onFocus="if(this.value == 'username') this.value = ''" required></p> <!-- JS because of IE support; better: placeholder="Username" -->
-        <p><span class="fontawesome-lock"></span><input type="password"  name="password" onBlur="if(this.value == '') this.value = 'password'" onFocus="if(this.value == 'password') this.value = ''" required></p> <!-- JS because of IE support; better: placeholder="Password" -->
+        <p><span class="fontawesome-user"></span><input type="text" name="username" onBlur="if(this.value == '') this.value = 'Username'" onFocus="if(this.value == 'Username') this.value = ''" required></p> <!-- JS because of IE support; better: placeholder="Username" -->
+        <p><span class="fontawesome-lock"></span><input type="password"  name="password" onBlur="if(this.value == '') this.value = 'Password'" onFocus="if(this.value == 'Password') this.value = ''" required></p> <!-- JS because of IE support; better: placeholder="Password" -->
         <p><input type="submit" value="Login"></p>
 
       </fieldset>
